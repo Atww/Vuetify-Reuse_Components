f6a353541d775cfb11e7a6cddd00ef29ca758556
Atw <35207756+Atww@users.noreply.github.com>
2020-09-01 10:25:22 +0700

    :lollipop: Table

---
+++ Table/TableExample.vue
@@ -0,0 +1,107 @@
+<template>
+  <v-card>
+    <v-card-title>
+      {{ nameTable }}
+
+      <v-btn
+        v-if="isInsertMore"
+        style="margin:20px"
+        color="primary"
+        small
+        @click="insertAction()"
+      >
+        <v-icon dark>
+          mdi-plus
+        </v-icon>
+      </v-btn>
+      <v-spacer />
+      <v-text-field
+        v-model="search"
+        append-icon="mdi-magnify"
+        label="Search"
+        single-line
+        hide-details
+      />
+    </v-card-title>
+    <v-data-table
+      :headers="header"
+      :items="datasource"
+      :search="search"
+      :loading="isLoading"
+      :loading-text="isLoadingTxt"
+    >
+      <template
+        v-for="slot in Object.keys($scopedSlots)"
+        :slot="slot"
+        slot-scope="scope"
+      >
+        <slot
+          :name="slot"
+          v-bind="scope"
+        />
+      </template>
+    </v-data-table>
+  </v-card>
+</template>
+
+<script>
+//   console.log(Object.keys($slots))
+
+  export default {
+    props: {
+      header: {
+        type: Array,
+        default: function () {
+          return []
+        },
+      },
+      datasource: {
+        type: Array,
+        default: function () {
+          return []
+        },
+      },
+      isAction: {
+        type: Boolean,
+        default: function () {
+          return false
+        },
+      },
+      isLoading: {
+        type: Boolean,
+        default: function () {
+          return false
+        },
+      },
+      isInsertMore: {
+        type: Boolean,
+        default: function () {
+          return false
+        },
+      },
+      isLoadingTxt: {
+        type: String,
+        default: function () {
+          return 'กำลังโหลดข้อมูลกรุณารอสักครู่'
+        },
+      },
+      nameTable: {
+        type: String,
+        default: () => '',
+      },
+      insertAction: {
+        type: Function,
+        default: () => {},
+      },
+    },
+    data () {
+      return {
+        search: '',
+      }
+    },
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
